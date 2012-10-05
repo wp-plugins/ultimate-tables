6925baf1034fb097c680766de20af58e3dff1c34
extendyourweb <extendyourweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-05 03:01:45 +0000

    git-svn-id: https://plugins.svn.wordpress.org/ultimate-tables/trunk@608162 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ template/footer.php
@@ -0,0 +1,39 @@
+<?php
+$site_url = get_option( 'siteurl' );
+?>
+<style type="text/css">
+<!--
+.style2 {color: #999999}
+.style3 {color: #666666}
+.style4 {
+	color: #D50948;
+	font-weight: bold;
+	font-style: italic;
+}
+.style5 {color: #D50948}
+.style6 {
+	border: 2px solid;
+	padding:10px;
+	text-align: center;
+	color: #D50948;
+	font-size: 12px;
+	font-style: italic;
+}
+-->
+</style>
+<hr />  
+
+   
+   
+  
+<ul>
+<li><img src="<?php echo plugins_url('', __FILE__); ?>/icono3.png" width="50" height="50" /> <span class="style5"><strong><em>Use</em></strong>. </span><span class="style3">Create a table. Then edit the table values ​​and settings by pressing the button "edit". You can select columns and rows in the table, as well as its design, filter, search and more. </span></li>
+</ul>
+<hr />
+<p class="style6"><a href="http://www.extendyourweb.com/wordpress/ultimate-tables/">Ultimate tables manual and samples</a> ||
+<a href="http://www.extendyourweb.com/wordpress/">Visit our page and see more premium plugins!</a></p>
+
+<p>&nbsp;</p>
+
+<hr>
+</div>

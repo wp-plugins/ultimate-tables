8b11949cc26e89d448a4dc1acea1ac7831cc4c16
extendyourweb <extendyourweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-11 12:00:58 +0000

    New version 1.3
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-tables/tags/1.3@946833 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ template/footer.php
@@ -0,0 +1,36 @@
+<?php
+$site_url = get_option( 'siteurl' );
+?>
+<style type="text/css">
+<!--
+.style2 {color: #999999}
+.style3 {color: #666666}
+.style4 {
+	color: #D50948;
+	font-weight: bold;
+	font-style: italic;
+}
+.style5 {color: #D50948}
+.style6 {
+	border: 2px solid;
+	padding:10px;
+	text-align: center;
+	color: #D50948;
+	font-size: 12px;
+	font-style: italic;
+}
+-->
+</style>
+<hr />  
+
+    <h2>USE</h2>
+   
+   <p> Click <span class="style5"><strong>edit</strong></span> to set the table.
+Add, edit, or delete the items you want to display in the table.</p>
+
+<hr />
+
+<table style="text-align:center;"><tr><td><a href="http://www.extendyourweb.com" target="_blank"><img src="<?php echo plugins_url('', __FILE__); ?>/extendyourweb.jpg" /></a></td></tr><tr valign="top"><td><a href="http://www.extendyourweb.com" target="_blank">DOWNLOAD MORE PLUGINS</a><p>Now, you can download our 7 best plugins for video and image and enjoy all our creations in your sites.</p></td></tr></table>
+
+
+</div>
